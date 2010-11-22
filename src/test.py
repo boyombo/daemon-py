f6629d6c1531f10c5c73c2b104e684c611c98774
Fredrick Galoso <fred@wayoutmind.com>
2010-11-22 14:47:26 -0600

    updated src/test.py to call sys.stdout directly rather than print

--- src/test.py
+++ src/test.py
@@ -7,25 +7,25 @@ import time
 from daemon import Daemon
 
 class TestDaemon(Daemon):
-	def run(self): #Define what tasks/processes to daemonize
-		while True:
-			time.sleep(1)
+    def run(self): #Define what tasks/processes to daemonize
+        while True:
+            time.sleep(1)
 
 if __name__ == "__main__":
-	daemon = TestDaemon('/tmp/daemon-py-test.pid') #Define pidfile location (typically /tmp or /var/run)
-	if len(sys.argv) == 2:
-		if 'start' == sys.argv[1]:
-			daemon.start()
-		elif 'stop' == sys.argv[1]:
-			daemon.stop()
-		elif 'restart' == sys.argv[1]:
-			daemon.restart()
-		elif 'status' == sys.argv[1]:
-		    daemon.status()
-		else:
-			print "Unknown command"
-			sys.exit(2)
-		sys.exit(0)
-	else:
-		print "Usage: %s start|stop|restart|status" % sys.argv[0]
-		sys.exit(2)
+    daemon = TestDaemon('/tmp/daemon-py-test.pid') #Define a pidfile location (typically located in /tmp or /var/run)
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        elif 'status' == sys.argv[1]:
+            daemon.status()
+        else:
+            sys.stdout.write("Unknown command\n")
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        sys.stdout.write("Usage: %s start|stop|restart|status\n" % sys.argv[0])
+        sys.exit(2)
